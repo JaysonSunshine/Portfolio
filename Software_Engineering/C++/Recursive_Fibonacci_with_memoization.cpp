e7dd4e7d52707da713e726cacff9ddb810f23a21
JaysonSunshine <jay.t.cloyd@gmail.com>
2015-01-10 18:08:55 -0800

    Recursive Fibonacci with Memoization

---
+++ Software_Engineering/C++/Recursive_Fibonacci_with_memoization.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+void fibhelp(int n, int end, vector<long long>* memo){
+	if(n > end)
+		return;
+	if(n == 1){
+		(*memo).push_back(1);
+		fibhelp(n + 1, end, memo);
+	}
+	else if(n == 2){
+		(*memo).push_back(1);
+		fibhelp(n + 1, end, memo);
+	}
+	else{
+		(*memo).push_back((*memo)[n - 3] + (*memo)[n - 2]);
+		fibhelp(n + 1, end, memo);
+	}
+}
+
+long long fib(int n){
+	if(n < 1)
+		return 0;
+	if(n == 1)
+		return 1;
+	if(n == 2)
+		return 1;
+	vector<long long> memo;
+	fibhelp(1, n, &memo);
+	return memo.back();
+}
+
+int main(int argc, char* argv[]){
+	cout << fib(atoi(argv[1])) << "\n";
+}
