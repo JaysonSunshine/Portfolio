beed75afeb3ee49b0198fccd015617dffee23e8f
JaysonSunshine <jay.t.cloyd@gmail.com>
2015-02-15 02:29:24 -0800

    Words.py

--- Software_Engineering/Python/Words.py
+++ Software_Engineering/Python/Words.py
@@ -1,11 +1,13 @@
 import sys
+import re
 
 def wordsplit(text):
 	f = open(text, 'r')
 	for line in f:
-		a = line.split()
+		a = re.split('\s|\[|\"|\.|\,|\:|\(|\)|\!|--|\?|\]|;|_|\/|#|&', line)
 		for word in a:
-			print word
+			if word != '':
+				print word
 
 def main(argv):
 	wordsplit(sys.argv[1])
